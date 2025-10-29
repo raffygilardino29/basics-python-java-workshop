686b5f84d83374cd96844eee89578bceda5c5d19
unknown <raffy.gilardino29@gmail.com>
2025-10-29 11:08:17 +0700

    program klarifikasi usia

--- python/challenges/2_condition_age.py
+++ python/challenges/2_condition_age.py
@@ -1,5 +1,16 @@
 # Program Klasifikasi Usia
 
 # 1. TODO: Baca input dari pengguna
-
+umur = int(input("masukan umur anda : "))
 # 2. TODO Tentukan kategori berdasarkan usia
+keterangan = ""
+if umur <= 12:
+    keterangan = "anak - anak"
+elif umur <= 20:
+    keterangan = "remaja"
+elif umur <= 30:
+    keterangan = "dewasa"
+else:
+    keterangan = "bau tanah"
+
+print(keterangan)
