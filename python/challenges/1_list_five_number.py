1000e497d953258e8f57e041b99d298bea89c16a
unknown <raffy.gilardino29@gmail.com>
2025-10-29 10:52:01 +0700

    Test Push

--- python/challenges/1_list_five_number.py
+++ python/challenges/1_list_five_number.py
@@ -1,15 +1,18 @@
 # 1. Buat list berisi 5 angka
-numbers = [10, 25, 7, 40, 15]
+numbers = [29, 26, 5, 45, 12]
 print("List awal:", numbers)
 
 # 2. TODO: Tampilkan angka pertama dan terakhir
-print("Angka pertama:")
-print("Angka terakhir:")
+print(f"Angka pertama: {numbers[0]} ")
+print(f"Angka terakhir: {numbers[-1]}")
 
 # 3. TODO: Tambahkan angka baru (apapun) ke akhir list
-print("List setelah ditambah:", numbers)
+numbers.append(22)
+print("List setelah ditambah:", numbers )
 
 # 4. TODO: Urutkan list secara descending
-
+numbers.sort()
+print("list setelah diurutkan :", numbers)
 # 5. Tampilkan hasil akhir
+numbers.sort(reverse=True)
 print("List urut descending:", numbers)
